8efc68d15efe99b3b2fc733bf889cdb014cc9285
abul-ala-jobayar-1 <jobayar59@gmail.com>
2023-03-31 13:20:01 +0600

    author-image-added

--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -4,10 +4,10 @@ const Main = ({data}) => {
     console.log(data)
     const {name, title,img,authorImg,date,readTime}=data
     return (
-        <div>
-           <img className='w-96 rounded-s' src={img} alt="no imgge show" />
+        <div className='container pl-10'>
+           <img className='w-96 rounded-s' src={img} alt="programming img" />
            <div>
-            
+           <img className='w-10 h-10 rounded-full' src={authorImg} alt="programming img" />
            </div>
            <h1></h1>
            <p></p>
